3e823b7fedf8d430d84001a4d0a5ec6ad5cf511d
a972667237 <wuezmonster@gmail.com>
2018-08-19 14:33:48 +0800

    init(gold): init all

---
+++ gold/index/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Comment_article, Encyclopedia_article
+
+# Register your models here.
+admin.site.register(Comment_article)
+admin.site.register(Encyclopedia_article)
---
+++ gold/index/models.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from django.db import models
+from DjangoUeditor.models import UEditorField
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(u'title', max_length=100)
+    content = UEditorField(u'content', max_length=200000)
+    date = models.DateField(u'create_date', auto_now_add=True, editable=True)
+    author = models.CharField(u'author', max_length=100)
+    time = models.DateField(u'publish_time')
+    class Meta:
+        abstract = True
+    def __unicode__(self):
+        return self.title
+
+class Comment_article(Article):
+    class Meta:
+        verbose_name = 'comment 评论'
+        verbose_name_plural = verbose_name
+
+class Encyclopedia_article(Article):
+    class Meta:
+        verbose_name = 'encyclopedia 百科'
+        verbose_name_plural = verbose_name
